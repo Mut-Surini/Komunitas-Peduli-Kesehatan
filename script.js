b715bc257a17c8612cc4aede592b54091539d8dc
Mutsurini <fitrahqareem99@gmail.com>
2023-11-04 15:02:50 +0700

    Patch 3

--- script.js
+++ script.js
@@ -126,7 +126,76 @@ function upComments(server, user, pernyataan){
     tambahKomentar(user, pernyataan);
 }
 
-upComments("Global", "Fitrah", "Aku juga mau");
+// upComments("Global", "Fitrah", "Aku juga mau");
+
+//Server Management
+
+let kode = 1234;
+
+function createServer(nama, user){
+    fs.mkdir(`./Server/${nama}`, { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+
+    const info = [{
+        "namaServer" : nama,
+        "kodeServer" : kode+1,
+        "admin" : user,
+        "users" : []
+    }];
+
+    const comments = [];
+
+    fs.writeFile(`./Server/${nama}/info.json`, JSON.stringify(info), (err) => {
+        if(err) throw err;
+        console.log('Server Create');
+    });
+
+    fs.writeFile(`./Server/${nama}/comments.json`, JSON.stringify(comments), (err) => {
+        if(err) throw err;
+    });
+
+    kode += 1;
+}
+
+function deleteServer(nama){
+
+    const path = require(`path`);
+
+    const deleteFolderRecursive = function (directoryPath) {
+    if (fs.existsSync(directoryPath)) {
+        fs.readdirSync(directoryPath).forEach((file, index) => {
+          const curPath = path.join(directoryPath, file);
+          if (fs.lstatSync(curPath).isDirectory()) {
+           // recurse
+            deleteFolderRecursive(curPath);
+          } else {
+            // delete file
+            fs.unlinkSync(curPath);
+          }
+        });
+        fs.rmdirSync(directoryPath);
+      }
+    };
+
+    deleteFolderRecursive(`./Server/${nama}`);
+}
+
+// createServer("Keluargaku", "Fitrah");
+// deleteServer("Keluargaku");
+
+// Comments Management
+
+function callComments(nama){
+    const comments = require(`./Server/${nama}/comments.json`);
+    
+    comments.forEach(e => {
+        console.log(`Nama : ${e.nama}
+        Komentar : ${e.comment}`);
+    });
+}
+
+callComments("Global");
 
 
 
