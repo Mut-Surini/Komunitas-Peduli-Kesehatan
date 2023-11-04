d5a7d21756430f5823caefcb366c3635a7cc8a5d
usernamedoesntexist <88772874+usernamedoesntexist@users.noreply.github.com>
2023-11-05 01:23:53 +0700

    fitrah ppk

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ const flash = require('connect-flash')
 faker.locale = 'id_ID'
 const Schema = mongoose.Schema
 const app = express()
+const server = require('http').createServer(app);
+
 const csvWriter = require('csv-write-stream');
 const fs = require('fs');
 const { decode } = require('punycode')
@@ -56,166 +58,6 @@ mongoose.connect('mongodb+srv://fajaradiputra127:fajar1234@cluster0.u62fkfd.mong
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-
-// =======================
-
-function upComments(server, user, pernyataan){
-
-    const urlServer = `./Server/${server}/comments.json`;
-    const serverComments = require(`./Server/${server}/comments.json`);
-
-    function createArray(pernyataan){
-        const array = pernyataan.split(' ');
-        return array;
-    }
-
-    function createArray2(pernyataan){
-        const array = pernyataan.split(' ');
-        const array2 = [];
-        for(i = 0; i < (array.length-1) ; i++){
-            array2.push(array[i] + ' ' + array[i+1]);
-        }
-        return array2;
-    }
-    
-    function createArray3(pernyataan){
-        const array = pernyataan.split(' ');
-        const array3 = [];
-        for(i = 0; i < (array.length-1) ; i++){
-            array3.push(array[i] + ' ' + array[i+1] + ' ' + array[i+2]);
-        }
-        return array3;
-    }
-    
-    function createArray4(pernyataan){
-        const array = pernyataan.split(' ');
-        const array4 = [];
-        for(i = 0; i < (array.length-1) ; i++){
-            array4.push(array[i] + ' ' + array[i+1] + ' ' + array[i+2] + ' ' + array[i+3]);
-        }
-        return array4;
-    }
-
-    function checkArray(array){
-
-        const result = [];
-    
-        array.forEach(e => {
-        penyakit.forEach(f => {
-            if(e.toLowerCase() == f.nama.toLowerCase()){
-                result.push(f.nama);   
-            }
-        });
-        }); 
-    
-        return result;
-    }
-    
-    function isiResult(arr){
-        arr.forEach(e => {
-            result.push(e);
-        });
-    }
-    
-    function tambahKomentar(nama, comment){
-    
-        const result = [];
-    
-        const arrayPernyataan = createArray(comment);
-        const arrayPernyataan2 = createArray2(comment);
-        const arrayPernyataan3 = createArray3(comment);
-        const arrayPernyataan4 = createArray4(comment);
-    
-        isiResult(checkArray(arrayPernyataan));
-        isiResult(checkArray(arrayPernyataan2));
-        isiResult(checkArray(arrayPernyataan3));
-        isiResult(checkArray(arrayPernyataan4));
-    
-        serverComments.push({
-            nama,
-            comment,
-            arrayComment : result
-        });
-    
-        fs.writeFile(urlServer, JSON.stringify(serverComments), function writeJSON(err) {
-            if (err) return console.log(err);
-            console.log("Add Comment Success");
-        });
-        
-    }
-
-    tambahKomentar(user, pernyataan);
-}
-
-// upComments("Global", "Fitrah", "Aku juga mau");
-
-//Server Management
-
-let kode = 1234;
-
-function createServer(nama, user){
-    fs.mkdir(`./Server/${nama}`, { recursive: true }, (err) => {
-        if (err) throw err;
-      });
-
-    const info = [{
-        "namaServer" : nama,
-        "kodeServer" : kode+1,
-        "admin" : user,
-        "users" : []
-    }];
-
-    const comments = [];
-
-    fs.writeFile(`./Server/${nama}/info.json`, JSON.stringify(info), (err) => {
-        if(err) throw err;
-        console.log('Server Create');
-    });
-
-    fs.writeFile(`./Server/${nama}/comments.json`, JSON.stringify(comments), (err) => {
-        if(err) throw err;
-    });
-
-    kode += 1;
-}
-
-function deleteServer(nama){
-
-    const path = require(`path`);
-
-    const deleteFolderRecursive = function (directoryPath) {
-    if (fs.existsSync(directoryPath)) {
-        fs.readdirSync(directoryPath).forEach((file, index) => {
-          const curPath = path.join(directoryPath, file);
-          if (fs.lstatSync(curPath).isDirectory()) {
-           // recurse
-            deleteFolderRecursive(curPath);
-          } else {
-            // delete file
-            fs.unlinkSync(curPath);
-          }
-        });
-        fs.rmdirSync(directoryPath);
-      }
-    };
-
-    deleteFolderRecursive(`./Server/${nama}`);
-}
-
-// createServer("Keluargaku", "Fitrah");
-// deleteServer("Keluargaku");
-
-// Comments Management
-
-function callComments(nama, kolom){
-    const comments = require(`./Server/${nama}/comments.json`);
-    
-    comments.forEach(e => {
-        console.log("Success");
-    });
-}
-
-// =======================
 
 const UserSchema = new Schema({
     name: {
@@ -314,7 +156,7 @@ app.post('/register',[body('username').isLength({min: 4}).withMessage('This fiel
     body('umur').isLength({min: 1}).withMessage('This field is required').isNumeric().withMessage('Masukkan angka yang valid!')],async(req,res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
-        return res.render('registration',{title: 'Blog - Register',layout: 'main-login-regis',data: req.body, errors: errors.array()})
+        return res.render('signup',{title: 'Blog - Register',layout: 'main-regis',data: req.body, errors: errors.array()})
     }
     const token = nanoid()
     await Token.create({token, email: req.body.email,type: 'activation'})
@@ -337,7 +179,7 @@ app.post('/login',[body('email').isLength({min: 1}).withMessage('This field is r
 }),body('password').isLength({min: 1}).withMessage('This field is required')],async(req,res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
-        return res.render('login',{title: 'Blog - Login', layout: 'main-login-regis', msg: req.flash('msg'),errors: errors.array(),data: req.body})
+        return res.render('sign',{title: 'Blog - Login', layout: 'main-login', msg: req.flash('msg'),errors: errors.array(),data: req.body})
     }
     const user = await User.findOne({email: req.body.email}).select('email name')
     // set jwt token
@@ -410,18 +252,6 @@ app.post('/auth/reset/:token',body('password').isLength({min: 1}).withMessage('T
     req.flash('msg','Your password has been changed please login')
     res.redirect('/login')
 })
-
-app.post('/createServer', (req, res) => {
-
-    createServer(req.body.nameServer, "User");
-
-    res.redirect(`/${req.body.nameServer}`);
-})
-
-app.post('/joinServer', (req, res) => {
-
-    res.redirect(`/${req.body.nameServer}`);
-})
 // End of Auth
 
 app.get('/', (req, res) => {
@@ -438,18 +268,18 @@ app.get('/register',(req,res,next) => {
     if(res.locals.login){
         return next()
     }
-    res.render('registration',{title: 'Blog - Register',layout: 'main-login-regis'})
+    res.render('signup',{title: 'Blog - Register',layout: 'main-regis'})
 })
 app.get('/login',(req,res) => {
     const token = req.cookies.access_token
     if(!token){
-        return res.render('login', {title: 'Blog - Login', layout: 'main-login-regis', msg: req.flash('msg')})
+        return res.render('sign', {title: 'Blog - Login', layout: 'main-login', msg: req.flash('msg')})
     }
     jwt.verify(token,'secret',(err,decoded) => {
         if(err){
             req.flash('msg','Your session has been expired')
             res.clearCookie('access_token')
-            return res.render('login', {title: 'Blog - Login', layout: 'main-login-regis', msg: req.flash('msg')})
+            return res.render('sign', {title: 'Blog - Login', layout: 'main-login', msg: req.flash('msg')})
         }else{
             return res.redirect('/')
         }
@@ -583,10 +413,6 @@ app.get('/global', (req, res) => {
     res.render('index',{layout: false});
 })
 
-app.get('/server', (req, res) => {
-    res.render('server',{layout: false});
-})
-
 app.use('/',(req,res) => {
     res.statusCode = 404
     res.render('404',{title: 'Not Found', layout: '404'})
@@ -594,6 +420,13 @@ app.use('/',(req,res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000..')
-})
+const io = require('socket.io')(server);
+io.on('connection', (socket) => { 
+    console.log('new user connected..')
+    socket.on('new_message', (data) => {
+        io.sockets.emit('new_message', {message: data.message})
+    })
+});
+server.listen(3001, () => {
+    console.log('Server listening on port 3001..')
+});
